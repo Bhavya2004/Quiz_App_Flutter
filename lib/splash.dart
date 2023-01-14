c78e4fbde61045e7c55bed210b76c9f0a3ef2e51
Bhavya Barai <94623439+Bhavya2004@users.noreply.github.com>
2023-01-14 23:27:30 +0530

    Login screen added

---
+++ lib/splash.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:quistar/home.dart';
+import 'package:quistar/login_page.dart';
+// import 'package:quizstar/home.dart';
+
+class splashscreen extends StatefulWidget {
+  @override
+  _splashscreenState createState() => _splashscreenState();
+}
+
+class _splashscreenState extends State<splashscreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 2), () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(
+        builder: (context) => LoginPage(),
+      ));
+    });
+  }
+
+  // added test yourself
+  // and made the text to align at center
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.indigo,
+      body: Center(
+        child: Text(
+          "Quizstar\nTest Your Skills !!",
+          style: TextStyle(
+            fontSize: 50.0,
+            color: Colors.white,
+            fontFamily: "Satisfy",
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
